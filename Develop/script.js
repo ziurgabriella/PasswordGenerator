315be90242b75f09acb95e94c6a6e8ab8ff61eb2
ziurgabriella <gabriellaruiz907@gmail.com>
2023-04-20 19:54:44 -0400

    determined lenght of password

--- Develop/script.js
+++ Develop/script.js
@@ -12,6 +12,24 @@ var uppercaseCheck;
 var numberCheck;
 var specialCheck;
 
+function determineLength(){
+  passwordLength = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
+
+    if (passwordLength<8){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>128){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else{
+    alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
+    }
+    return passwordLength;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
