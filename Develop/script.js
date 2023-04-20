c300990b89138226e4b60d34e829de6bc956d497
ziurgabriella <gabriellaruiz907@gmail.com>
2023-04-20 19:50:30 -0400

    began to write code on JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,13 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordLength;
+var uppercaseCheck;
+var numberCheck;
+var specialCheck;
 
 
 // Get references to the #generate element
